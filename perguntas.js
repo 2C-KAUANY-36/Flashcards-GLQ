bc0c7867a16b73c1e59fef609dfb51b74ce52378
2C-KAUANY-36 <00001132240487sp@al.educacao.sp.gov.br>
2024-11-24 15:36:25 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Quantos estados tem no Brasil?',
+    'O Brasil tem 26 estados.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Australia?',
+    'A capital da Australia é Canberra.'
+)
+
+criaCartao(
+    'Química',
+    'Quantos elementos tem na tabela periódica?' ,
+    'Na tabela periótica tem 118 elementos.'
+)
+
+criaCartao(
+    'Lingua Portuguesa',
+    'Quantas classes gramaticais existem?',
+    'Existem na língua Portuguesa 10 classes gramaticais.'
+)
